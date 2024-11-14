dc8da9d632f77877239fc3f8d1e60e4a51fe190e
kumarkse <abhishektiwari2100@gmail.com>
2024-11-14 23:28:08 +0530

    Golang

---
+++ hello.go
@@ -0,0 +1,20 @@
+package main
+
+//import "fmt"
+
+func main() {
+	/*fmt.Println("hello world")
+	  fmt.Println("11"+"2w")
+	  fmt.Println(11+11)
+	  fmt.Println(11+11.11)
+	  fmt.Println(21.0/3)
+	  fmt.Println(true)
+	  fmt.Println(true && false)
+	  var ab="55"
+	  fmt.Println(ab)
+	  var ab  ,bc =3,5
+	  fmt.Println(ab,bc)
+	  ab:="43.22"
+	  fmt.Println(ab)
+	*/
+}
