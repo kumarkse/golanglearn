a83d74cd75f67b91733f5ff204b466ef3a1bcbef
kumarkse <abhishektiwari2100@gmail.com>
2024-12-18 01:23:30 +0530

    jsOneshot

---
+++ JSoneshot/appp.js
@@ -0,0 +1,55 @@
+// alert('hello');
+console.log("hello");
+
+let score =50;
+score =90
+console.log(score)
+
+const score2 = 50;
+// score2=10;   error
+
+let name = "hello"
+let age= 30
+rating = 4.5
+x = true
+y = null
+z = undefined
+let beta
+
+console.log(typeof(beta))
+console.log('myname'+age+'hello d')
+console.log(`my age is ${age}`)
+
+let str = 'hello world'
+
+console.log(str.length)
+console.log(str.substring(0,5))
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+console.log(str.split(''))
+
+let numbers = new Array(1,2,5,5,2)
+console.log(numbers)
+let fruit = ["hello",52,true,undefined]
+fruit[7]="end"
+fruit.push("pushed")
+fruit.unshift('0110')
+fruit.pop()
+console.log(fruit)
+
+console.log(Array.isArray(fruit))
+
+console.log(fruit.indexOf(undefined))
+
+
+let person = {
+    name : "fname",
+    addr : "addresss"
+}
+
+console.log(person)
+console.log(person.addr)
+
+person.email = "ad@ii"
+
+console.log(person)
